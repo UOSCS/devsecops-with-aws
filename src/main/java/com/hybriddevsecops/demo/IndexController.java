9ac757dc558dee7b547e57a6201c90016476386d
EC2 Default User <ec2-user@ip-172-31-43-74.ap-southeast-2.compute.internal>
2022-07-11 02:49:59 +0000

    sast test

--- src/main/java/com/hybriddevsecops/demo/IndexController.java
+++ src/main/java/com/hybriddevsecops/demo/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    
+    private final SAMPLE_ACCESS_KEY = ML686RDFZUWOHYHCQQTG;
+    
+    private final SAMPLE_SECRET_KEY = 65goffb7GXeE1e/dTPVrEm26LHGvr3M4LoXg5/pj;
 
     @GetMapping("/")
     public String sayHello(Model model) {
